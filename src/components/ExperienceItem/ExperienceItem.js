fab38b0bd1c0b7fa1ab08af8ea3eb1d5296d5732
Marcelo Dapper <marcelo.dapper@gmail.com>
2017-03-31 22:40:45 -0300

    Add Experience and Education page
    Update Experience component
    Create ExperienceItem component
    Create ExperienceWrap component
    Create IconEducation and IconWork icons

---
+++ src/components/ExperienceItem/ExperienceItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './ExperienceItem.css';
+
+const ExperienceItem = props => (
+  <div className={styles.item}>
+    <div className={styles.icon}>
+      <div className={styles.iconWrap}>
+        {props.icon}
+      </div>
+    </div>
+    <div className={styles.description}>
+      <p className={styles.period}>{props.period}</p>
+      <h4 className={styles.company}>{props.company}</h4>
+      <h5 className={styles.position}>{props.position}</h5>
+    </div>
+  </div>
+)
+
+export default ExperienceItem;
---
+++ src/components/ExperienceWrap/ExperienceWrap.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ExperienceWrap.css';
+
+const ExperienceWrap = props => (
+  <div className={styles.wrap}>
+    {props.children}
+  </div>
+)
+
+export default ExperienceWrap;
