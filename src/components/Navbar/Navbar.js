518cd8f708bc6bbe22f9c6eb81aca981a14b15d0
Marcelo Dapper <marcelo.dapper@gmail.com>
2017-04-03 11:46:57 -0300

    Add page Contact and style Navbar for mobile
    Add styeles and template for component Contact
    Add styeles and template for component ContactForm
    Fix component ContainerFull to max-width 1900px
    Create icon component IconContact
    Create icon component IconExperience
    Create icon component IconHome
    Create icon component IconProjects
    Create icon component IconSkills
    Fix component Navbar to work on mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,11 @@ import styles from './Navbar.css';
 
 import Container from '../Container/Container';
 import Row from '../Row/Row';
+import IconHome from '../Icons/IconHome';
+import IconSkills from '../Icons/IconSkills';
+import IconProjects from '../Icons/IconProjects';
+import IconExperience from '../Icons/IconExperience';
+import IconContact from '../Icons/IconContact';
 
 const Navbar = () => (
   <nav className={styles.nav}>
@@ -13,22 +18,37 @@ const Navbar = () => (
           <NavLink exact to="/" className={styles.brand} href=""><img src="./img/Logo.svg" alt="logo"/></NavLink>
         </div>
 
-        <div className={styles.navbar__menu}>
-          <ul className={styles.navbar__nav}>
+        <div className={styles.menu}>
+          <ul className={styles.menuList}>
             <li className={styles.item}>
-              <NavLink exact to="/">Home</NavLink>
+              <NavLink exact to="/">
+                <span className={styles.icon}><IconHome/></span>
+                <span className={styles.label}>Home</span>
+              </NavLink>
             </li>
             <li className={styles.item}>
-              <NavLink to="/skills">Skills</NavLink>
+              <NavLink to="/skills">
+                <span className={styles.icon}><IconSkills/></span>
+                <span className={styles.label}>Skills</span>
+              </NavLink>
             </li>
             <li className={styles.item}>
-              <NavLink to="/projects">Projects</NavLink>
+              <NavLink to="/projects">
+                <span className={styles.icon}><IconProjects/></span>
+                <span className={styles.label}>Projects</span>
+              </NavLink>
             </li>
             <li className={styles.item}>
-              <NavLink to="/experience">Experience</NavLink>
+              <NavLink to="/experience">
+                <span className={styles.icon}><IconExperience/></span>
+                <span className={styles.label}>Experience</span>
+              </NavLink>
             </li>
             <li className={styles.item}>
-              <NavLink to="/contact">Contact</NavLink>
+              <NavLink to="/contact">
+                <span className={styles.icon}><IconContact/></span>
+                <span className={styles.label}>Contact</span>
+              </NavLink>
             </li>
           </ul>
         </div>
